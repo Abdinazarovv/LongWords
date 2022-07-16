5fa52facf3087d0952bc6b8d183f9ea53b30b0fb
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-07-16 18:09:16 +0300

    first commit:

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var text = "Abdinazarov Islombek International house lyceum student";
+
+function LongWord(letters) {
+    var newArr = letters.split(" ");
+    var longsWord = "";
+    
+    for (var i = 0; i < newArr.length; i++) {
+        
+        if (newArr[i].length > longsWord.length) {
+            longsWord = newArr[i];
+        }
+    }
+    return longsWord;
+}
+var a = LongWord(text);
+console.log(a);
